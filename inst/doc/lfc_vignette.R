404d9225f063683e66aafaeee50c059fa57fd2d9
Florian Erhard <florian.erhard@uni-wuerzburg.de>
2023-04-19 17:10:02 +0000

    version 0.2.3

--- inst/doc/lfc_vignette.R
+++ inst/doc/lfc_vignette.R
@@ -1,24 +1,24 @@
 ## ----fig.height=5, fig.width=5, message=FALSE, warning=FALSE------------------
 library(DESeq2)
 library(lfc)
-data(airway, package="airway")
-A <- assay(airway)[,2]
-B <- assay(airway)[,1]
+
+# it is overly complicated to read a gzipped table from the internet. This is how it is done:
+t <- read.delim(textConnection(readLines(gzcon(url("https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE160764&format=file&file=GSE160764%5FRNA%5FMEF%5FOla%5FIFNb%5FCounttable%5FRaw%2Etxt%2Egz")))))
+
+A <- t[,4]
+B <- t[,2]
 
 ll <- PsiLFC(A,B)
-plot(ecdf(ll),xlim=c(-1,1),xlab="Log2 fold change treated/untreated",ylab="Cumulative frequency",main="Cell N61311",col='blue')
+plot(ecdf(ll),xlim=c(-1,1),xlab="Log2 fold change treated/untreated",ylab="Cumulative frequency",col='blue')
 lines(ecdf(CenterMedian(log2((A+1)/(B+1)))),col='red')
 
 ## ----fig.width=5, fig.height=5------------------------------------------------
-plot(ecdf(ll[A>0 | B>0]),xlim=c(-1,1),xlab="Log2 fold change treated/untreated",ylab="Cumulative frequency",main="Cell N61311",col='blue')
+plot(ecdf(ll[A>0 | B>0]),xlim=c(-1,1),xlab="Log2 fold change treated/untreated",ylab="Cumulative frequency",col='blue')
 lines(ecdf(CenterMedian(log2((A+1)/(B+1))[A>0 | B>0])),col='red')
 
-## -----------------------------------------------------------------------------
-head(PsiLFC.se(airway,contrast=c("dex","untrt","trt")))
-
 ## ----message=FALSE, warning=FALSE---------------------------------------------
-dds <- DESeqDataSetFromMatrix(countData = assay(airway),colData = colData(airway),design= ~ dex)
+dds <- DESeqDataSetFromMatrix(countData = t[,2:5],colData = data.frame(IFN=c("0h","0h","1h","1h")),design= ~ IFN)
 dds <- DESeq(dds)
-res <- results(dds, contrast=c("dex","untrt","trt"),cre=TRUE)
+res <- results(dds, contrast=c("IFN","1h","0h"),cre=TRUE)
 head(res)
 
